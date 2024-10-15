534c1d4a887ee50608e4ea6fcda3cf5bf1737562
Matthew Tan <sleepyboy123@hotmail.com>
2024-10-15 20:58:31 +0800

    youtube increased their price to almost 0

--- telegram_sender.py
+++ telegram_sender.py
@@ -12,13 +12,14 @@ chase_users = ["tanzyzy", "vcwd96", "sleepyboy123"]
 
 current_month = datetime.now().strftime("%B")
 
-application_cost_dict = {"youtube": 18}
+application_cost_dict = {"youtube": 28}
 application_cost = application_cost_dict[application] / len(chase_users)
 
 async def main():
     async with TelegramClient(StringSession(string_session), api_id, api_hash) as client:
         for user in chase_users:
             await client.send_message(user, "Hello, this is an automated reminder to transfer ${} to Matthew Tan for the {} {} subscription. Thanks!".format(application_cost, current_month, application))
+            await client.send_message(user, "This is getting expensive, please help me find more people :(")
         await client.send_message("me", "Hello, the automated reminders has been sent out for {} {} subscription".format(current_month, application))
 
 asyncio.run(main())
